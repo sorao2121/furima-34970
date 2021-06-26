65783560542d1def02895507464c8f076fd18632
sorao2121 <h.sorawaki@gmail.com>
2021-06-26 18:58:03 +0900

    テストコードの実装_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable
 
   validates :nickname,               presence: true
-  validates :encrypted_password,     presence: true
-  validates :family_name,            presence: true
-  validates :family_name_kana,       presence: true
-  validates :first_name,             presence: true
-  validates :first_name_kana,        presence: true
+  validates :encrypted_password,     presence: true, length: { minimum: 6 }
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :family_name_kana,       presence: true,format: {with: /\A[ァ-ヶー－]+\z/ } 
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :first_name_kana,        presence: true,format: {with: /\A[ァ-ヶー－]+\z/ } 
   validates :birth_date,             presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
